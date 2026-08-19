a643172e944fa5adc7508f14a8ffe38c33081d40
neu <neu@local>
2026-08-19 17:06:39 +0700

    Add REST proxy route test

---
+++ internal/gateway/rest_test.go
@@ -0,0 +1,28 @@
+package gateway
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/neu/go-kafka-neu/internal/storage"
+)
+
+func TestRESTProxyPublishRoute(t *testing.T) {
+	store, err := storage.NewStore(t.TempDir(), 1024*1024, 4096)
+	if err != nil {
+		t.Fatal(err)
+	}
+	srv := httptest.NewServer(NewRESTProxy(store).Handler())
+	defer srv.Close()
+
+	resp, err := http.Post(srv.URL+"/topics/rp/messages", "application/json", strings.NewReader(`{"value":{"x":1}}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 201 {
+		t.Fatalf("POST publish status: got %d want 201", resp.StatusCode)
+	}
+}
